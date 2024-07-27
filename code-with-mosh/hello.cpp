565eb7aa67767fa2b8fc8ea844d052c3294c3f2f
tufaelkhan <tufaelkhan247@gmail.com>
2024-07-27 09:56:12 +0600

    add c++

---
+++ code-with-mosh/hello.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // int a = 10;
+    // int b = 20;
+    // int c = a;
+    // a = b;
+    // b = c;
+    // cout<<a<<" "<<b;
+
+    // int x = 10;
+    // int y = 4;
+    // int z = x%y;
+    // cout<<++x;
+
+    // int x = (1+2)*3;
+    // cout<<x;
+
+    double x = 10;
+    double y = 5;
+    // double z = (x+10)/(3*y);
+    // cout<<z;
+    cout<<"x = "<<x<<endl;
+    cout<<"y = "<<y;
+    return 0;
+}
