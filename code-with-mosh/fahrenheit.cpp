9aea3c60014f0b10230bdff9d59748c782276bdb
tufaelkhan <tufaelkhan247@gmail.com>
2024-07-28 10:30:33 +0600

    add celsius

---
+++ code-with-mosh/fahrenheit.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"Enter fahrenheit : ";
+    int fahrenheit;
+    cin>>fahrenheit;
+    double celsius = (fahrenheit - 32)/1.8;
+    cout<<celsius;
+    
+    return 0;
+}
--- code-with-mosh/hello.cpp
+++ code-with-mosh/hello.cpp
@@ -17,11 +17,33 @@ int main(){
     // int x = (1+2)*3;
     // cout<<x;
 
-    double x = 10;
-    double y = 5;
+    // double x = 10;
+    // double y = 5;
     // double z = (x+10)/(3*y);
     // cout<<z;
-    cout<<"x = "<<x<<endl;
-    cout<<"y = "<<y;
+    // cout<<"x = "<<x<<endl;
+    // cout<<"y = "<<y;
+
+    // tax calculate;
+    // double sales = 950000;
+    // cout<<"Sales = $ "<<sales<<endl;
+
+    // const double stateTaxRate = .04;
+    // double stateTax = sales * stateTaxRate;
+    // cout<<"State Tax = $ "<<stateTax<<endl;
+
+    // const double countyTaxRate = .02;
+    // double countyTax = sales * countyTaxRate;
+    // cout<<"County Tax = $ "<<countyTax<<endl;
+
+    // double totalTax = stateTax + countyTax;
+    // cout<<"Total Tax = $ "<<totalTax;
+
+    cout<<"Enter x and y integer values: ";
+    double x, y;
+    cin>>x;
+    cin>>y;
+    cout<<x+y;
+
     return 0;
 }
