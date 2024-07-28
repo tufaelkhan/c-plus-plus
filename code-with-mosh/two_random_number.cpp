b1218a3f53ea74bdf255db328459d3fab907e670
tufaelkhan <tufaelkhan247@gmail.com>
2024-07-28 22:01:24 +0600

    add new content

--- code-with-mosh/two_random_number.cpp
+++ code-with-mosh/two_random_number.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main(){
-    
+    const short maxValue = 6;
+    const short minValue = 1;
+    srand(time(0));
+    short first = (rand()%(maxValue - minValue + 1)) + minValue;
+    short second = (rand()%(maxValue - minValue + 1)) + minValue;
+    cout << first <<","<<second;
     return 0;
 }
