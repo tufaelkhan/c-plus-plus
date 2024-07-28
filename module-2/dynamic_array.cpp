7e51000220dcb6d38c812ed259ff355e406bf2c1
tufaelkhan <tufaelkhan247@gmail.com>
2024-07-28 22:04:49 +0600

    Add .vscode and module-2 directories

---
+++ module-2/dynamic_array.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int *arr = new int[5];
+    for(int i = 0; i<5; i++){
+        cin>>arr[i];
+    }
+    for(int i = 0; i<5; i++){
+        cout<<arr[i]<<" "<<endl;
+    }
+
+    return 0;
+}
---
+++ module-2/dynaminc_data.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // int *a = new int;
+    // *a = 40;
+    // cout<<*a<<endl;
+
+    float *tu = new float;
+    *tu = 40.8234;
+    cout<<*tu;
+    return 0;
+}
